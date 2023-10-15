34a923b85b7616d921f8489aa505091b6011d1ea
Praveen <praveenriemplfizh@gmail.com>
2023-10-15 20:05:02 +0530

    commit - code updated for changing password

--- users/models.py
+++ users/models.py
@@ -12,12 +12,15 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save() #runs parent's class's save method
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs) #runs parent's class's save method
 
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
             output_size = (300,300)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+
+            rgb_im = img.convert('RGB')
+
+            rgb_im.save(self.image.path)
